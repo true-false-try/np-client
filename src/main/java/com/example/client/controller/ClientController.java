dc87e4c2ef7348bbe04b6188b7c73e28c0f2566a
true-false-try <6mateo6ch@gmail.com>
2022-10-16 23:16:57 +0300

    finished client and server logic

--- src/main/java/com/example/client/controller/ClientController.java
+++ src/main/java/com/example/client/controller/ClientController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.Response;
 import org.apache.tomcat.util.http.ResponseUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -25,21 +26,25 @@ import java.util.Random;
 public class ClientController {
     private final RestTemplate restTemplate;
 
+    @Value("${url.callback.document.create}")
+    private String URL_CALLBACK_DOCUMENT_CREATE;
+    @Value("${url.document.create}")
+    private String URL_DOCUMENT_CREATE;
+
     @Scheduled(cron = "${cron.request.period}")
-    public void getCreateDocument() {
-        log.info(String.format("[%s] Client generated request", ClientController.class.getName()));
+    public void generateClientRequest() {
+        log.info("Client generated request");
         Document document = Document.builder()
                 .number(new Random().nextInt(1000))
                 .type(new Random().nextInt(5))
-                .callbackUrl("http://localhost:8080/callback/document/create")
+                .callbackUrl(URL_CALLBACK_DOCUMENT_CREATE)
                 .build();
-        restTemplate.postForObject("http://localhost:8030/document/create", document, String.class);
+        restTemplate.postForObject(URL_DOCUMENT_CREATE, document, String.class);
     }
 
     @PostMapping("/callback/document/create")
-    public ResponseEntity<String> createOKStatus(@RequestBody String req) {
-        log.info(req);
-        log.info("HTTP Response Code ".concat(HttpStatus.OK.toString()));
+    public ResponseEntity<String> createDocumentCallback(@RequestBody Document document) {
+        log.info(document.toString());
         return ResponseEntity.ok().build();
     }
 }
